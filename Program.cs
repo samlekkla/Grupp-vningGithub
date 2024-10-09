0bbced876e1df2937c04506e904f9085ec902e44
Sam Lekkla <151582157+samlekkla@users.noreply.github.com>
2024-10-09 13:37:44 +0200

    Sams commit

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ Console.WriteLine("Hello, World!");
 
 string text1 = "Hello";
 string text2 = "World!";
+int tal1 = 1;
+int tal2 = 2;
+
 
 string result = text1 + text2;
+int resultInt = tal1 + tal2;
 
 Console.WriteLine(result);
+Console.WriteLine(resultInt);
