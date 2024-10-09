4d083ff744f9b94bf11770624761bf19e32d10df
Mairaj <Mairaj@DESKTOP-8USSVD5>
2024-10-09 13:32:34 +0200

    First commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");s
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Thanks Sam, First commit.");
